4381601159f7877e5d7b6f2504468af7b8a2272a
hub-mm <165568972+hub-mm@users.noreply.github.com>
2024-06-21 17:03:01 +0700

    Sort flex out in JS and size of divs.

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 const container = document.querySelector('.container');
+container.style.height = '100vh';
+container.style.width = '100%';
 container.style.display = 'flex';
-container.style.justifyContent = 'space-evenly'
+container.style.justifyContent = 'center'
+container.style.gap = '5px'
 
 for (let i = 1; i <= 16; i++) {
     const row = document.createElement('div');
     container.appendChild(row);
     row.style.display = 'flex';
     row.style.flexDirection = 'column';
-    row.style.justifyContent = 'space-evenly';
+    row.style.justifyContent = 'center';
+    row.style.gap = '5px';
 
     for (let j = 1; j < 16; j++) {
         const div = document.createElement('div');
-        div.style.border = 'solid black 2px'
-        div.style.padding = '5px'
-        div.style.margin = '5px'
-        div.textContent = 'div';
+        div.style.border = 'solid black 2px';
+        div.style.height = '50px';
+        div.style.width = '50px';
         row.appendChild(div);
     }
 }
